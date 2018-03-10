747d436835d16e92ac6d54e0e4f74c0479d4a068
wangm <501981732@qq.com>
2018-03-10 17:41:36 +0800

    按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+const login = r => require.ensure([], () => r(require('@/page/login')), 'login');
 
 Vue.use(Router)
 
-// 组件
-const login = r => require.ensure([],() =>r(require('@/page/login')),'login')
-
-
-
-
-
-
-
-const router = [
-    {
-        path: '/',
-        component: 'login'
-    }
-]
 export default new Router({
-    router,
-    // strict: process.env.NODE_ENV !== 'production',
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    }
+  ],
+  
 })
+
