cfc5afe8b9ed19eb158efcbf6a7aabf28f8de35a
weizhongjia <948913886@qq.com>
2018-05-14 14:41:44 +0800

    分别打包

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -46,7 +46,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: utils.assetsPath('css/[name].[contenthash].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
       allChunks: true,
     }),
@@ -81,8 +81,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'addPages.html',
       inject: true,
       chunks :'addTest',
-      // excludeChunks : ['app','vendor','manifest'],
-      // inlineSource : '.(js|css)$' 
+      excludeChunks : ['app','vendor','manifest'],
+      // inlineSource : '.(js|css)$'
       minify: {
         // removeComments: true,
         // collapseWhitespace: true,
